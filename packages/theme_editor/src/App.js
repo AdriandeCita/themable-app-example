06a093dfc031b675099c89610669dce7018c5d22
Mykola Myslovskyi <mykola.myslovskyi@eliftech.com>
2019-11-27 04:25:58 +0200

    rework on top of another boilerplate
    thus make shared components loadable

--- packages/theme_editor/src/App.js
+++ packages/theme_editor/src/App.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter, Link } from 'react-router-dom';
 import TextPage from './pages/text';
 import HomePage from './pages/home';
 
 function App() {
 	return (
-		<div>
-			<header className="App-header">Theme Editor</header>
-			<div className="main-layout">
-				<nav>
-					<h3>Components</h3>
-					<p>Typography</p>
-					<p>Image</p>
-					<p>Button</p>
-					<p>List</p>
-				</nav>
-				<main className="workspace-area">
-					<Route path="/" exact component={HomePage} />
-					<Route path="/text" exact component={TextPage} />
-				</main>
+		<BrowserRouter>
+			<div>
+				<header className="App-header">Theme Editor</header>
+				<div className="main-layout">
+					<nav>
+						<h3>Components</h3>
+						<ul>
+							<li>
+								<Link to="/text">Typography</Link>
+							</li>
+							<li>
+								<Link to="/">Image</Link>
+							</li>
+							<li>
+								<Link to="/">Button</Link>
+							</li>
+							<li>
+								<Link to="/">List</Link>
+							</li>
+						</ul>
+					</nav>
+					<main className="workspace-area">
+						<Route path="/" exact component={HomePage} />
+						<Route path="/text" exact component={TextPage} />
+					</main>
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 }
 
